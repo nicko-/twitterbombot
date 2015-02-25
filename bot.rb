f3de4951452dbae36a1acd5c11186ee92db0d02b
nick <nick@nxk.me>
2015-02-25 23:45:49 +1100

    first working version

--- bot.rb
+++ bot.rb
@@ -1,4 +1,5 @@
 require 'twitter'
+require 'json'
 require_relative 'config.rb'
 
 client = Twitter::REST::Client.new do |config|
@@ -8,4 +9,17 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = TWITTER_ACCESS_SECRET
 end
 
-client.update 'Testing testing 123...'
+weather_data = `curl #{WEATHER_URL}`
+raise 'weather download error' if weather_data.lines.length < 4
+weather_data = weather_data.lines.map {|line| line.split('#')}
+region_data = weather_data.select {|region| region[1] == WEATHER_REGION}[0]
+raise 'weather region error' if region_data.nil?
+
+if File.exists? 'tomorrow.json'
+  json = JSON.parse File.read('tomorrow.json')
+  client.update "#{json['conditions']} with a high of #{json['high']} degrees."
+end
+
+File.open 'tomorrow.json', 'w' do |file|
+  file.write JSON.generate('conditions': region_data[23].chomp('.').chomp, 'high': region_data[9])
+end
